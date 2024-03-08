faaed1f6dfae94d1d292bcbdb0b8cefcfe3a1fe5
aameli-kalkhoran <159202414+aameli-kalkhoran@users.noreply.github.com>
2024-03-08 13:24:03 -0500

    Update assign1.py

--- assign1.py
+++ assign1.py
@@ -12,9 +12,8 @@ with anyone or anything except for submission for grading.
 I understand that the Academic Honesty Policy will be enforced and 
 violators will be reported and appropriate action will be taken.
 
-Description: <Enter your documentation here>
-18773033013 
-Date: 
+Description: <aameli-kalkhoran>
+Date: 08-March 2024 
 '''
 
 
